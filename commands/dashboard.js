6147235cc2dbe58714fd68df143f0cc94901f036
OfficialSamBear <152440129+OfficialSamBear@users.noreply.github.com>
2023-12-02 03:40:49 -0500

    Update dashboard.js

--- commands/dashboard.js
+++ commands/dashboard.js
@@ -4,6 +4,6 @@ module.exports = class extends Command {
   name = 'dashboard';
   
   execute(msg) {
-    msg.channel.send(`The Dashboard Is At https://mainstreamgg.repl.co`);
+    msg.channel.send(`The Dashboard Is At https://name of bot repl.co`);
   }
 }
